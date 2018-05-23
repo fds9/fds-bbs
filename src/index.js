ea52b580301b6f1881a43ba84ac1b6bbf63e31a6
Kim Seungha <seungha.me@gmail.com>
2018-05-23 12:33:12 +0900

    코드 정리

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import './index.scss';
+import axios from 'axios';
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+const rootEl = document.querySelector('.root');
+const templates = {
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content,
+}
+
+async function indexPage() {
+  const res = await axios.get('http://localhost:3000/posts');
+  const listFragment = document.importNode(templates.postList, true);
+
+  res.data.forEach(post => {
+    const fragment = document.importNode(templates.postItem, true);
+    const pEl = fragment.querySelector('.post-item__title');
+    pEl.textContent = post.title;
+    listFragment.querySelector('.post-list').appendChild(fragment);
+  })
+
+  rootEl.appendChild(listFragment);
+}
+
+indexPage();
